085ae1286250c66dc68e783d217cadd1d341e0a2
Anna Dmitrieva <annakinkajou@outlook.com>
2017-03-19 19:10:05 +0500

    comment1

---
+++ ЛабРаб7/Middleware.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ЛабРаб7
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Middleware
+    {
+        private readonly RequestDelegate _next;
+
+        public Middleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            string path = httpContext.Request.Path.Value.ToLower();
+            if (path == "/test1")
+            {
+                await httpContext.Response.WriteAsync("You have opened the page test1");
+            }
+            else if (path == "/test2")
+            {
+                await httpContext.Response.WriteAsync("You have opened the page test2");
+            }
+            else
+            {
+                httpContext.Response.StatusCode = 404;
+                await httpContext.Response.WriteAsync("Path not found");
+            }  
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<Middleware>();
+        }
+    }
+}
---
+++ ЛабРаб7/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ЛабРаб7
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            //установка консоли для вывода лога
+            loggerFactory.AddConsole();
+
+            //если приложение в процессе разработки
+            if (env.IsDevelopment())
+            {
+                //то выводим информацию об ошибке, при наличии ошибки
+                app.UseDeveloperExceptionPage();
+            }
+
+            //обработка запроса - получаем контекст запроса в виде объекта contex 
+            //app.Run(async (context) =>
+            //{
+            //    //отправка ответа в виде строки "Hello World!"
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+            app.UseStaticFiles();
+            app.UseMiddleware<Middleware>();
+        }
+    }
+}
